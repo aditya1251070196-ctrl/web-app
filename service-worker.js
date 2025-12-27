8df82ae49258d6bb7854a31f9c062bcbe07d35e4
aditya1251070196-ctrl <aditya.1251070196@vit.edu>
2025-12-27 18:35:18 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "traffic-sign-cache-v1";
+const urlsToCache = [
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./model/model.json",
+  "./model/labels.json",
+  "./model/traffic_sign_model.weights.bin",
+  "./js/tf.min.js"
+];
+
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
