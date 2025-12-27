290656dcb7cdbef60bb388e9898f54687e330298
aditya1251070196-ctrl <aditya.1251070196@vit.edu>
2025-12-27 18:59:17 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,56 @@
-const CACHE_NAME = "traffic-sign-cache-v1";
-const urlsToCache = [
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./model/model.json",
-  "./model/labels.json",
-  "./model/traffic_sign_model.weights.bin",
-  "./js/tf.min.js"
+const CACHE_NAME = 'traffic-sign-pwa-v2';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './service-worker.js',
+  './tf.min.js',
+
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+
+  './model/model.json',
+  './model/traffic_sign_model.weights.bin',
+  './model/labels.json'
 ];
 
-
-self.addEventListener("install", event => {
+// INSTALL
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+      console.log('[SW] Caching assets');
+      return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+// ACTIVATE
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('[SW] Deleting old cache:', key);
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request);
     })
   );
 });
-
